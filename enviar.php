620cb95187a6681dfce62c93143a311d6c84b5ad
dankzito <gabrieltbeneitez@gmail.com>
2023-09-12 20:01:06 +0000

    hola

---
+++ enviar.php
@@ -0,0 +1,16 @@
+<?php 
+$servername = "localhost";
+$username = "";
+
+
+$name= $_POST["nombre"];
+$name= $_POST["apellido"];
+$name= $_POST["DNI"];
+$tel=  $_POST["telefono"];
+$name= $_POST["direccion"];
+if($_POST){
+echo("hola");
+
+
+}
+?>
