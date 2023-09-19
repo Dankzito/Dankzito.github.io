79a018bb023e7cdc1038851555cb2c07877efb8d
dankzito <gabrieltbeneitez@gmail.com>
2023-09-19 19:35:57 +0000

    hola

--- enviar.php
+++ enviar.php
@@ -1,16 +1,28 @@
-<?php 
+<?php
 $servername = "localhost";
-$username = "";
+$username = "phpmyadmin";
+$password = "RedesInformaticas";
+$dbname = "db_beneitez";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Error de conexión: " . $conn->connect_error);
+}
+$nombre = $_POST["nombre"];
+$apellido = $_POST["apellido"];
+$dni = $_POST["DNI"];
+$tel = $_POST["telefono"];
+$direccion = $_POST["direccion"];
 
+$sql = "INSERT INTO registro (nombre, apellido, DNI, telefono, direccion) VALUES ('$nombre', '$apellido', '$dni', '$tel', '$direccion')";
 
-$name= $_POST["nombre"];
-$name= $_POST["apellido"];
-$name= $_POST["DNI"];
-$tel=  $_POST["telefono"];
-$name= $_POST["direccion"];
-if($_POST){
-echo("hola");
-
-
+if ($_POST) {
+  if ($conn->query($sql) === TRUE) {
+    echo "Registro exitoso";
+  } else {
+    echo "Error al registrar: " . $conn->error;
+  }
+  
+  // Cerrar la conexión a la base de datos
+  $conn->close();
 }
 ?>
